6d7a17d5685c037df7b391bed5c0d579c01c31c1
Simon-157 <boatengsimonjnr157@gmail.com>
2025-03-28 08:27:45 +0000

    session error

--- backend/src/middleware/cors.middleware.ts
+++ backend/src/middleware/cors.middleware.ts
@@ -1,11 +1,10 @@
 import type { CorsOptions } from "cors";
 
 const cors: CorsOptions = {
-  origin: process.env.NODE_ENV === "development" ? process.env.DEV_CLIENT_URL?.trim() : process.env.CLIENT_URL?.trim(),
+  origin: process.env.NODE_ENV === "development" ? process.env.DEV_CLIENT_URL : process.env.CLIENT_URL,
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"],
-  exposedHeaders: ["Set-Cookie"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
 
 export { cors };
