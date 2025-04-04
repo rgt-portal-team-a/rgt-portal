badedeff5d01e3bdd91d6e1f66c03d104ff87ee4
Simon Junior Boateng <79936608+Simon-157@users.noreply.github.com>
2025-04-04 09:25:04 +0000

    Update ai.controller.ts

--- backend/src/controllers/ai.controller.ts
+++ backend/src/controllers/ai.controller.ts
@@ -48,7 +48,7 @@ export class AiController {
     }
   }
 
-  // KAIRO CHATBOT API WHICH TAKES A QUERY FROM FORMS AND RETURNS A RESPONSE
+  // KAIRO CHATBOT 
   async kairoChatbot(req: Request, res: Response): Promise<void> {
     try {
       const { query } = req.body;
@@ -84,7 +84,6 @@ export class AiController {
         throw new Error(`Candidate with ID ${candidate_id} not found`);
       }
 
-      // Format dates to YYYY-MM-DD
       const formatDate = (date: Date | null | undefined): string | null => {
         if (!date) return null;
         return date instanceof Date ? date.toISOString().split('T')[0] : new Date(date).toISOString().split('T')[0];
@@ -105,7 +104,6 @@ export class AiController {
       const response = await axios.post(`${process.env.AI_API_ENDPOINT}/predict-dropoff`, requestData); 
       console.log(response.data);
 
-      // update the candidates's predicted dropoff
       candidate.predictedDropOff = response?.data?.dropoff_probability;
       await AppDataSource.getRepository(Recruitment).save(candidate);
 
@@ -157,7 +155,6 @@ export class AiController {
         applied_position: candidate.position || candidate.firstPriority,
       });
 
-      // update the candidates's predicted score
       candidate.predictedScore = response?.data?.match_score;
       await AppDataSource.getRepository(Recruitment).save(candidate);
 
